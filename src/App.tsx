1e82673294880432f4a9c06e3e26ea477be25912
Andrey Povstyanko <werty.potom@gmail.com>
2022-09-25 01:36:41 +0600

    deleted comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 
 import Files from './components/Files';
-import FileUpload from './components/FileUpload';
 import GraphPresentation from './components/Graph';
 
 function App() {
-  const onClickNode = function (nodeId: string) {
-    // window.alert(`Clicked node ${nodeId}`);
-  };
-
-  const onClickLink = function (source: string, target: string) {
-    window.alert(`Clicked link between ${source} and ${target}`);
-  };
-
   return (
     <>
       <Files />
--- src/components/Files.tsx
+++ src/components/Files.tsx
@@ -20,7 +20,6 @@ const Files = () => {
       });
 
       const data = (await responce.json()) as IFile[];
-      console.log('data ', data);
 
       setFiles(data);
     } catch (error) {
